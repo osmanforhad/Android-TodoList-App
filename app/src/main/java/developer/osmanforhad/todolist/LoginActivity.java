c529953893d902fdc60e2b5a465a958ff0836a67
Osman Forhad <osmanforhad93@gmail.com>
2022-04-27 03:23:12 +0600

    linking Login and Register layout files with java files

--- app/src/main/java/developer/osmanforhad/todolist/LoginActivity.java
+++ app/src/main/java/developer/osmanforhad/todolist/LoginActivity.java
@@ -1,11 +1,23 @@
 package developer.osmanforhad.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //__Define the Variables__//
+    private Toolbar toolbar;
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private TextView loginQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,29 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_login);
+
+        //__Initiate the Variables and Connect With XML UI Id__//
+        toolbar = (Toolbar) findViewById(R.id.loginToolbar);
+        loginEmail = (EditText) findViewById(R.id.loginEmail);
+        loginPassword = (EditText) findViewById(R.id.loginPassword);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginQuestion = (TextView) findViewById(R.id.loginPageQuestion);
+
+        //__Setup Toolbar as Actionbar__//
+        setSupportActionBar(toolbar);
+        //__Setup App Screen Title__//
+        getSupportActionBar().setTitle("Login");
+
+        //__Setup Click Event in to the loginQuestion TextView__//
+        loginQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //__Go to the Next Screen__//
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/developer/osmanforhad/todolist/RegistrationActivity.java
+++ app/src/main/java/developer/osmanforhad/todolist/RegistrationActivity.java
@@ -1,11 +1,23 @@
 package developer.osmanforhad.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    //__Define the Variables__//
+    private Toolbar toolbar;
+    private EditText registerEmail, registerPassword;
+    private Button registerButton;
+    private TextView registerQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,29 @@ public class RegistrationActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_registration);
+
+        //__Initiate the Variables and Connect With XML UI Id__//
+        toolbar = (Toolbar) findViewById(R.id.registrationToolbar);
+        registerEmail = (EditText) findViewById(R.id.registrationEmail);
+        registerPassword = (EditText) findViewById(R.id.registrationPassword);
+        registerButton = (Button) findViewById(R.id.registrationButton);
+        registerQuestion = (TextView) findViewById(R.id.registrationPageQuestion);
+
+        //__Setup Toolbar as Actionbar__//
+        setSupportActionBar(toolbar);
+        //__Setup App Screen Title__//
+        getSupportActionBar().setTitle("Registration");
+
+        //__Setup Click Event in to the RegistrationQuestion TextView__//
+        registerQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //__Go to the Next Screen__//
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
